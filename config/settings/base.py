f4fde4dbe29b6fed6afc145bba2a90e6e64936f6
ventti <ehdgus2357@gmail.com>
2021-01-17 23:08:08 +0900

    로깅 적용

--- config/settings/base.py
+++ config/settings/base.py
@@ -173,6 +173,10 @@ LOGGING = {
             'level': 'INFO',
             'propagate': False,
         },
+        'pybo': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
     }
 }
 
